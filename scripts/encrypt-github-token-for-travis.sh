156a4049822d7bf14954a845e07003b9446331d2
Wolfram Kriesing <wk@uxebu.com>
2015-02-26 22:11:29 +0100

    The script to generate the travis secret.

---
+++ scripts/encrypt-github-token-for-travis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Usage: `basename $0` repo_name github_token";
+  echo "";
+  echo "repo_slug     Is the <github_username>/<repo_name>, e.g. tddbin/tddbin-frontend";
+  echo "github_token  go to https://github.com/settings/applications#personal-access-tokens ";
+  echo "              to get the token needed and pass it in here.";
+  echo "";
+  exit 1;
+fi
+
+travis-encrypt -r $1 GH_TOKEN=$2
