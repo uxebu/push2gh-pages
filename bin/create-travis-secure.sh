be73eade63565523d343a7f1fe6fb555cc6e9277
Wolfram Kriesing <wk@uxebu.com>
2015-03-03 23:03:11 +0100

    Make script work in deployed mode too.

--- bin/create-travis-secure.sh
+++ bin/create-travis-secure.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-ROOT_DIR="$(dirname $0)/.."
-$ROOT_DIR/scripts/encrypt-github-token-for-travis.sh $@
+# in dev mode
+if [ -f ../scripts/encrypt-github-token-for-travis.sh ]
+then
+  ../scripts/encrypt-github-token-for-travis.sh $@
+else
+  # as installed npm package, located inside 'node_modules/push2gh-pages'
+  if [ -f ../push2gh-pages/scripts/encrypt-github-token-for-travis.sh ]
+  then
+    ../push2gh-pages/scripts/encrypt-github-token-for-travis.sh $@
+  fi
+fi
